5b213acc13409c2eb8beeb3559d5885f985acbda
Jeremy Perez <glyde06@hotmail.com>
2017-05-13 22:17:59 -0500

    used yarn to add material-ui and react-tap-event-plugin (a dependency of material-ui at the moment).

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+// Needed for onTouchTap 
+// http://stackoverflow.com/a/34015469/988941
+import injectTapEventPlugin from 'react-tap-event-plugin';
+ 
+injectTapEventPlugin();
 
 ReactDOM.render(
   <App />,
