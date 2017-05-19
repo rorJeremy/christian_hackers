bd404f7d9a31f5a7e293778e97ac59a7f6f6c34d
Jeremy Perez <glyde06@hotmail.com>
2017-05-19 13:19:12 -0500

    nvm. I'm not going to use Semantic-ui. #Indecisive.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar';
-// import { Input, Menu, Container } from 'semantic-ui-react'
-import {Menu, Segment, Icon } from 'semantic-ui-react'
 
 class App extends Component {
-  state = { activeItem: 'home' }
-
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
-
   render() {
-    const { activeItem } = this.state
-
     return (
-      <Navbar></Navbar>
-    )
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
   }
 }
 
