9c2b47e17d72b2985d3aeadb4ded3c63c0c16777
Jeremy Perez <glyde06@hotmail.com>
2017-05-25 17:08:49 -0500

    introducing reducers

---
+++ src/reducers/reducer_events.js
@@ -0,0 +1,9 @@
+// A Reducer is just a function that returns a piece of the application’s state.
+export default function() {
+  return [
+    { title: 'Event1', description: 'First Event' },
+    { title: 'Event2', description: 'Second Event' },
+    { title: 'Event3', description: 'Third Event' },
+    { title: 'Event4', description: 'Fourth Event' }
+  ];
+}
