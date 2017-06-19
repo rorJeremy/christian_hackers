61da68c559605ed76ea80c9e94390ab51bae2f8c
jeremy perez <glyde06@hotmail.com>
2017-06-18 21:33:44 -0500

    just some comments.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,8 @@ import { combineReducers } from 'redux';
 import EventsReducer from './reducer_events';
 
 const rootReducer = combineReducers({
+	// We use the name of the piece of state 'events' as the key
+	// We use the reducer 'EventsReducer' as the value
   events: EventsReducer
 });
 
