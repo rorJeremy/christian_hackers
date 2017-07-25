c18e3fd58664a05f7e96e2a6ba31fcc3c27c2cbf
jeremy perez <glyde06@hotmail.com>
2017-07-24 20:49:04 -0500

    refactored the actioncreator to pull from an now.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,16 @@
-export function selectEvent(event) {
+import axios from "axios";
+export const FETCH_EVENTS = 'fetch_events';
+
+const ROOT_URL = "http://localhost:3003/api/v1";
+
+export function fetchEvents() {
   // selectEvent is an ActionCreator, it needs to return an action,
   // An action is an object with a type property.
   // console.log('An event has been selected', event.title);
+  const request = axios.get(`${ROOT_URL}/events`);
+
   return {
-    type: "EVENT_SELECTED",
-    payload: event
+    type: "FETCH_EVENTS",
+    payload: request
   };
 }
